a8e47fe3c81d900fdc4eb3c192b78408f42f2ca1
ramyashetty24 <Ramya1@ibm.com>
2025-12-26 16:57:27 +0530

    Create zap_setup.sh

---
+++ scripts/zap_setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "OWASP API and UI scans need some level of setup for application repos"
+echo "For a working example, see https://github.ibm.com/one-pipeline/hello-compliance-app/blob/master/scripts/zap/trigger_zap_scans"
+
+exit 1
