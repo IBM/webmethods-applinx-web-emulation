82717137bd019b3d2d4908d0b7eccad765f8d8d4
ramyashetty24 <Ramya1@ibm.com>
2026-01-12 15:21:52 +0530

    Update zap_setup.sh

--- scripts/zap_setup.sh
+++ scripts/zap_setup.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
-
-echo "saving artifact image"
 set -euo pipefail
 
+echo "saving artifact image"
 echo "pwd"
 pwd
 
 echo "Login to ICR registry"
-# Login to uk.icr.io with dev-applinx-icr namespace
-# Using environment variables for credentials (not hardcoded)
 docker login uk.icr.io -u "$(get_env applinx-icrio-docker-user)" -p "$(get_env dastscan-api-key)"
 
-# Set branch/tag to pull
 BRANCH=main
 
 echo "docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH"
 docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH
 
-# Image details
 IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation"
 DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH | awk -F@ '{print $2}')"
 IMAGE_TAG=$BRANCH
@@ -25,8 +20,6 @@ IMAGE_TAG=$BRANCH
 echo "save-artifact"
 save_artifact app-image \
     type=image \
-    name="uk.icr.io/dev-applinx-icr/applinx-web-emulation" \
-    "digest=${DIGEST}" \
-    "tags=${IMAGE_TAG}"
-
-    ---------------------------------
+    name="${IMAGE}" \
+    digest="${DIGEST}" \
+    tags="${IMAGE_TAG}"
