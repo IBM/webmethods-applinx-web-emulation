0932d26abb5d05cbab1e89426670be0b19f82b1b
ramyashetty24 <Ramya1@ibm.com>
2026-01-12 14:44:59 +0530

    Update zap_setup.sh

--- scripts/zap_setup.sh
+++ scripts/zap_setup.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-echo "OWASP API and UI scans need some level of setup for application repos"
-echo "For a working example, see https://github.ibm.com/one-pipeline/hello-compliance-app/blob/master/scripts/zap/trigger_zap_scans"
+echo "saving artifact image"
+set -euo pipefail
 
-exit 1
+echo "pwd"
+pwd
+
+echo "Login to ICR registry"
+# Login to uk.icr.io with dev-applinx-icr namespace
+# Using environment variables for credentials (not hardcoded)
+docker login uk.icr.io -u "$(get_env applinx-icrio-docker-user)" -p "$(get_env dastscan-api-key)"
+
+# Set branch/tag to pull
+BRANCH=main
+
+echo "docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH"
+docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH
+
+# Image details
+IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation"
+DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH | awk -F@ '{print $2}')"
+IMAGE_TAG=$BRANCH
+
+echo "save-artifact"
+save_artifact app-image \
+    type=image \
+    name="uk.icr.io/dev-applinx-icr/applinx-web-emulation" \
+    "digest=${DIGEST}" \
+    "tags=${IMAGE_TAG}"
+
+    ---------------------------------
