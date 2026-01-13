9dea34b4182b8e6780f6e1d8588fce806df1fb1c
librogil-ibm <gil.librowski@ibm.com>
2026-01-13 08:57:44 +0200

    fixing hebrew opposite direction error (2)

--- src/app/mini-components/field/field.component.ts
+++ src/app/mini-components/field/field.component.ts
@@ -20,10 +20,9 @@ import { GXUtils } from 'src/utils/GXUtils';
 import {Field} from '@ibm/applinx-rest-apis';
 
 @Component({
-    selector: 'app-field',
-    templateUrl: './field.component.html',
-    styleUrls: ['./field.component.scss'],
-    standalone: false
+  selector: 'app-field',
+  templateUrl: './field.component.html',
+  styleUrls: ['./field.component.scss']
 })
 export class FieldComponent implements OnChanges {
 
