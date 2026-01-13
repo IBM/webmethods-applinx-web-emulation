33121696647ffe567ace69c4f62e1bf0fa8e6dc9
ramyashetty24 <Ramya1@ibm.com>
2026-01-13 11:08:14 +0530

    Update zap_setup.sh

--- scripts/zap_setup.sh
+++ scripts/zap_setup.sh
@@ -11,10 +11,10 @@ docker login uk.icr.io/dev-applinx-icr -u "$(get_env applinx-icrio-docker-user)"
 BRANCH=v1
 
 echo "docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH"
-docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH
+docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:26a6c048be66a0550938ea42dea4891c93de70f276e4076263dcd47aead55806
 
-IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation"
-DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH | awk -F@ '{print $2}')"
+IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:26a6c048be66a0550938ea42dea4891c93de70f276e4076263dcd47aead55806"
+DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' $IMAGE | awk -F@ '{print $2}')"
 IMAGE_TAG=$BRANCH
 
 echo "save-artifact"
