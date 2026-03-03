411dc2496764b1ddf59ed0695db58448c6bbd283
ramyashetty24 <Ramya1@ibm.com>
2026-03-03 12:17:24 +0530

    Update zap_setup.sh

--- scripts/zap_setup.sh
+++ scripts/zap_setup.sh
@@ -8,12 +8,12 @@ pwd
 echo "Login to ICR registry"
 docker login uk.icr.io/dev-applinx-icr -u "$(get_env applinx-icrio-docker-user)" -p "$(get_env dastscan-api-key)"
 
-BRANCH=v1
+BRANCH=v2
 
 echo "docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation:$BRANCH"
-docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:ab3742200ff9127a1b97518c0b79fcf7ff2145300976c3d6e40e356f4bf73479
+docker pull uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:ba68c50d333e1fd360f9d2406633e64025f7c1624516bf6b2f4cf846fb6e1815
 
-IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:ab3742200ff9127a1b97518c0b79fcf7ff2145300976c3d6e40e356f4bf73479"
+IMAGE="uk.icr.io/dev-applinx-icr/applinx-web-emulation@sha256:ba68c50d333e1fd360f9d2406633e64025f7c1624516bf6b2f4cf846fb6e1815"
 DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' $IMAGE | awk -F@ '{print $2}')"
 IMAGE_TAG=$BRANCH
 
